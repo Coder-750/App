7cfaad6cd94ea20e86acb44df8d665eeeff0841f
CODER750-LUCARIO--IN-SL <78160165+Coder-750@users.noreply.github.com>
2021-08-10 16:22:06 +0530

    Update App.cs

--- App.cs
+++ App.cs
@@ -1,1 +1,39 @@
+using System;
+using NativeUiLib;
 
+namespace CounterApp
+{
+	public class Program
+	{
+		public void Main()
+		{
+			Ui.RunOnUiThread(()=> MainUi());
+		}
+		
+		private void MainUi()
+		{
+			var count=1;
+			var linear = new LinearLayout();
+			var btn = linear.AddButton(true);
+			var txt = linear.AddTextView(true);
+			var myApp = linear.AddTextView(true);
+			myApp.Text="~Made By Hattori Sololearner!";
+			myApp.SetTextColor(Android.Graphics.Color.Tan);
+			txt.Text=count.ToString();
+			txt.SetTextSize(Android.Util.ComplexUnitType.Sp,50);
+			btn.Text = "Hey Click Me To Increment!";
+			btn.SetTextColor(Android.Graphics.Color.Teal);
+			btn.SetBackgroundColor(Android.Graphics.Color.LightPink);
+			
+			btn.Click+=delegate{
+			count+=1;
+			btn.Text="Thanks For Clicking!";
+		    txt.Text=count.ToString();
+			
+		};
+		
+		linear.Show();
+		
+	}
+}
+} 
